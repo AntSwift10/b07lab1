3ea8514c37f4b21962f648946d08762609e4baf8
AntSwift10 <antswift10@gmail.com>
2024-05-27 14:55:54 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -3,37 +3,73 @@ import java.lang.Math;
 public class Polynomial {
 	//Fields
 	double [] coefficients;
-	double [] exponents;
+	int [] exponents;
 	
 	//Methods
 	Polynomial() {
 		coefficients = new double[1];
 		coefficients[0] = 0;
-		exponents = new double[1];
+		exponents = new int[1];
 		exponents[0] = 0;
 	}
 	
-	Polynomial(double[] coeffs, double[] exps) {
+	Polynomial(double[] coeffs, int[] exps) {
 		coefficients = new double[coeffs.length];
 		for(int i = 0; i < coeffs.length; i++) {
 			coefficients[i] = coeffs[i];
 		}
-		exponents = new double[exps.length];
+		exponents = new int[exps.length];
 		for(int i = 0; i < exps.length; i++) {
 			exponents[i] = exps[i];
 		}
 	}
 	
 	Polynomial add(Polynomial added) {
-	//Get length of new Exponent Array
-		int len = 0;
+		//Make a temporary array
+		int max = 0;
+		int temp = Math.max(exponents[exponents.length-1], added.exponents[added.exponents.length-1]) + 1;
+		double [] tempCoeffs = new double[temp];
+		for (int i = 0; i < temp; i++) {
+			tempCoeffs[i] = i;
+		}
 		
+		//Add to the Temp array
+		for (int i = 0; i < exponents.length; i++) {
+			tempCoeffs[exponents[i]] = coefficients[i];
+		}
+		
+		for (int i = 0; i < added.exponents.length; i++) {
+			tempCoeffs[added.exponents[i]] += added.coefficients[i];
+		}
+		
+		//Convert to Final Array
+		int len = 0;
+		for (int i = 0; i < temp; i++) {
+			if (tempCoeffs[i] != 0) {
+				len++;
+			}
+		}
+		
+		double [] newCoeffs = new double[len];
+		int [] newExponents = new int[len];
+		
+		int j = 0;
+		for (int i = 0; i < temp; i++) {
+			if (tempCoeffs[i] != 0) {
+				newCoeffs[j] = tempCoeffs[i];
+				newExponents[j] = i;
+				j++;
+			}
+		}
+		
+		//Return Result
+		return new Polynomial(newCoeffs, newExponents);
 	}
 	
 	double evaluate(double point) {
 		double result = 0;
 		for(int i = 0; i < coefficients.length; i++) {
-			result += coefficients[i] * Math.pow(point, i);
+			result += coefficients[i] * Math.pow(point, exponents[i]);
 		}
 		return result;
 	}
