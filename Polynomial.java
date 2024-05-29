b292d0f1e3787e40ec7457e791edf76114744f6e
AntSwift1 <antswift10@gmail.com>
2024-05-28 22:42:47 -0400

    Fixed a constructor bug

--- Polynomial.java
+++ Polynomial.java
@@ -20,7 +20,7 @@ public class Polynomial {
 	
 	Polynomial(double[] coeffs, int[] exps) {
 		int max = 0;
-		for (int i = 0; i < exponents.length; i++) {
+		for (int i = 0; i < exps.length; i++) {
 			max = Math.max(max, exps[i]);
 		}
 		
